c3a92c6882d65fb12ac5a993ab96319e93889206
Roman Shapiro <eleroman@mail.ru>
2020-02-23 01:43:26 +0700

    Build fix

--- copy_zip_package_files.bat
+++ copy_zip_package_files.bat
@@ -4,12 +4,9 @@ rmdir "ZipPackage" /Q /S
 rem Create required folders
 mkdir "ZipPackage"
 mkdir "ZipPackage\MonoGame"
-mkdir "ZipPackage\FNA"
 
 set "CONFIGURATION=Release\net45"
 
 rem Copy output files
-copy "src\SpriteFontPlus\bin\MonoGame\%CONFIGURATION%\SpriteFontPlus.dll" "ZipPackage\MonoGame" /Y
-copy "src\SpriteFontPlus\bin\MonoGame\%CONFIGURATION%\SpriteFontPlus.pdb" "ZipPackage\MonoGame" /Y
-copy "src\SpriteFontPlus\bin\FNA\%CONFIGURATION%\SpriteFontPlus.dll" "ZipPackage\FNA" /Y
-copy "src\SpriteFontPlus\bin\FNA\%CONFIGURATION%\SpriteFontPlus.pdb" "ZipPackage\FNA" /Y
+copy "src\bin\MonoGame\%CONFIGURATION%\SpriteFontPlus.dll" "ZipPackage\MonoGame" /Y
+copy "src\bin\MonoGame\%CONFIGURATION%\SpriteFontPlus.pdb" "ZipPackage\MonoGame" /Y
